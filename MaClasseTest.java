406f1c82f89f3ad2927621b5eb4ff944ce7ff06d
Ibrahima Ba Anne <ibrahimaba.anne@insoftsas.com>
2021-12-21 11:05:25 +0000

    Add U-JUNIT TEST

---
+++ MaClasseTest.java
@@ -0,0 +1,9 @@
+import junit.framework.*;
+
+public class MaClasseTest extends TestCase {
+
+  public void testCalculer() throws Exception {
+    
+    assertEquals(2,MaClasse.calculer(1,1));
+  }
+}
